c6835b16ef970d9da7baa39793e740119f9e2db0
Hermes José Pérez Rojas <hermesjose94@gmail.com>
2018-08-29 14:14:46 -0400

    Pirmer commit

---
+++ database/migrations/2018_08_29_000000_create_empresa_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresaTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $set_schema_table = 'empresa';
+
+    /**
+     * Run the migrations.
+     * @table empresa
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable($this->set_schema_table)) return;
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->comment('Llave foranea de la empresa');
+            $table->string('name', 45)->comment('Nombre de la empresa');
+            $table->string('email', 45)->nullable()->comment('Correo electronico de la empresa');
+            $table->string('logo', 45)->nullable()->comment('Logotipo de la empresa');
+            $table->string('web', 45)->nullable()->comment('Sitio web de la empresa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->set_schema_table);
+     }
+}
---
+++ database/migrations/2018_08_29_000001_create_empleados_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $set_schema_table = 'empleados';
+
+    /**
+     * Run the migrations.
+     * @table empleados
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable($this->set_schema_table)) return;
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('nombre', 45);
+            $table->string('apellido', 45);
+            $table->unsignedInteger('empresa')->comment('Llave foranea con empresa');
+            $table->string('email', 45)->nullable();
+            $table->integer('telefono')->nullable();
+
+            $table->index(["empresa"], 'fk_empleados_empresa_idx');
+
+
+            $table->foreign('empresa', 'fk_empleados_empresa_idx')
+                ->references('id')->on('empresa')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+     public function down()
+     {
+       Schema::dropIfExists($this->set_schema_table);
+     }
+}
